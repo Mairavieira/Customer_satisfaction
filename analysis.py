f832f4918080c02be88f4e97a5282dc1e6c50748
Maíra Vieira <66793206+Mairavieira@users.noreply.github.com>
2022-07-28 11:46:15 -0300

    python

--- analysis.py
+++ analysis.py
@@ -32,3 +32,43 @@ sellers.head()
 # 8 - product translation
 product_translation = pd.read_csv(diretorio + 'product_category_name_translation.csv')
 product_translation
+
+#Merge all consumer datasets
+A = pd.merge(orders, order_reviews, on='order_id')
+A = pd.merge(A, order_payments,on='order_id')
+A = pd.merge(A, customer,on='customer_id')
+A.head()
+
+#Merge all seller datasets 
+B = pd.merge(order_items,products,on='product_id')
+B = pd.merge(B,sellers,on='seller_id')
+B = pd.merge(B,product_translation,on='product_category_name')
+B.head()
+
+#Merge consumer and seller datasets
+df_ecommerce = pd.merge(A, B, on = 'order_id')
+df_ecommerce.head()
+
+#Checking column names from the resulting dataset
+df_ecommerce.columns
+
+#Selecting desired columns and renaming
+df_ecommerce = df_ecommerce[['order_status','order_purchase_timestamp','order_delivered_customer_date','order_estimated_delivery_date',
+                              'shipping_limit_date','payment_sequential','payment_type','payment_installments','payment_value',
+                              'price','freight_value','product_category_name_english','product_name_lenght','product_description_lenght',
+                               'product_photos_qty','review_score']]
+
+
+df_ecommerce = df_ecommerce.rename(columns={'product_category_name_english': 'product_category'})
+
+#Checking the number of rows and columns of the resulting dataset
+df_ecommerce.shape
+
+#removing columns with NULL value
+df_ecommerce.isnull().sum()
+df_ecommerce.dropna(how='any',inplace=True)
+
+#Converting data and time columns
+cols = ['order_purchase_timestamp', 'order_estimated_delivery_date', 'order_delivered_customer_date', 'shipping_limit_date']
+for col in cols:
+    df_ecommerce[col] = pd.to_datetime(df_ecommerce[col]).dt.date
